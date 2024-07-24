b9c4bd5d19c42ee73eaf6c4b5fbbfe4d3eb74531
unlie9 <unlie.devops9@gmail.com>
2024-07-24 11:15:00 +0300

    created views index and tag, basic frontend for site

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,11 @@ from django.urls import (
     path,
     include
 )
+from app.views import IndexView, TagListView
 
 urlpatterns = [
-
+    path("", IndexView.as_view(), name='index'),
+    path("tags/", TagListView.as_view(), name='tags'),
 ]
 
 app_name = 'app'
--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from app.models import Task, Tag
+
+
+class IndexView(ListView):
+    model = Task
+    template_name = "todo_list/index.html"
+
+
+class TagListView(ListView):
+    model = Tag
+    template_name = "todo_list/tag.html"
+

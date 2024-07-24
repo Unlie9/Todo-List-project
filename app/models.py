3d74a69c8c84439af9524964da7ecf031a49ab21
unlie9 <unlie.devops9@gmail.com>
2024-07-24 10:18:50 +0300

    create models and apply migrations

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Task(models.Model):
+    content = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag)
---
+++ app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import (
+    path,
+    include
+)
+
+urlpatterns = [
+
+]
+
+app_name = 'app'
